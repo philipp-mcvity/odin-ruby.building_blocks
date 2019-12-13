26181c57b703eadf411be50a9e3a3fac4dcbd7db
Philipp Bischoff <bischoff.phil@gmail.com>
2019-12-13 12:31:44 +0100

    add substrings > completed file

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,2 +1,18 @@
 #!/usr/bin/ruby
 # frozen_string_literal: true
+
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+def substrings(string, dictionary)
+  str_ary = string.downcase.gsub(/[^a-z0-9 ']/, '').split
+  substring_count = Hash.new(0)
+  str_ary.each do |substr|
+    dictionary.each do |word|
+      substring_count[word] += 1 if substr.match(word)
+    end
+  end
+  puts substring_count
+end
+
+substrings('below', dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
